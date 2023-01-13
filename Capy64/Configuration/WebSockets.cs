719b66fdb0128587281d8d04f3184bc689a24d92
Alessandro Proto <me@alexdevs.me>
2023-01-13 21:37:42 +0100

    Basis for configuration

---
+++ Capy64/Configuration/WebSockets.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capy64.Configuration;
+
+class WebSockets
+{
+    public bool Enable { get; set; } = true;
+    public int MaxActiveConnections { get; set; } = 5;
+}
--- Capy64/Program.cs
+++ Capy64/Program.cs
@@ -1,10 +1,23 @@
 ﻿using Capy64;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.IO;
 
 using var game = new Capy64.Capy64();
 using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, c) =>
+    {
+        var settingsPath = Path.Combine(Capy64.Capy64.AppDataPath, "settings.json");
+        if (!File.Exists(settingsPath))
+        {
+            File.Copy("Assets/default.json", settingsPath);
+        }
+
+        c.AddJsonFile("Assets/default.json", false);
+        c.AddJsonFile(settingsPath, false);
+    })
     .ConfigureServices((hostContext, services) =>
     {
         services.AddSingleton<IGame>(game);
