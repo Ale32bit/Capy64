ecad4df4ad3b51e77042aa45a446525ae61417f6
Alessandro Proto <me@alexdevs.me>
2023-01-13 23:48:03 +0100

    Config template for HTTP

---
+++ Capy64/Configuration/HTTP.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capy64.Configuration;
+
+class HTTP
+{
+    public bool Enable { get; set; } = true;
+    public string[] Blacklist { get; set; }
+    public WebSockets WebSockets { get; set; }
+}
