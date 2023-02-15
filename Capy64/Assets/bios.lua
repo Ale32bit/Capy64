6cce5467498201f371cb28f37601294e05f98bb4
Alessandro Proto <me@alexdevs.me>
2023-02-15 14:26:04 +0100

    Add LICENSE to top of each source file

--- Capy64/Assets/bios.lua
+++ Capy64/Assets/bios.lua
@@ -1,4 +1,13 @@
-﻿local term = require("term")
+﻿-- This file is part of Capy64 - https://github.com/Capy64/Capy64
+-- Copyright 2023 Alessandro "AlexDevs" Proto
+--
+--    Licensed under the Apache License, Version 2.0 (the "License").
+--    you may not use this file except in compliance with the License.
+--    You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+
+local term = require("term")
 local timer = require("timer")
 local gpu = require("gpu")
 local fs = require("fs")
--- Capy64/Runtime/Objects/WebSocketClient.cs
+++ Capy64/Runtime/Objects/WebSocketClient.cs
@@ -1,4 +1,13 @@
-﻿using Capy64.API;
+﻿// This file is part of Capy64 - https://github.com/Capy64/Capy64
+// Copyright 2023 Alessandro "AlexDevs" Proto
+//
+//    Licensed under the Apache License, Version 2.0 (the "License").
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+
+using Capy64.API;
 using Capy64.Runtime.Libraries;
 using KeraLua;
 using System;
