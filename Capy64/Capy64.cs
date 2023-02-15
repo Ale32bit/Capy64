f7591e0d356b39e534296fd7c94a7fde5078ec21
Alessandro Proto <me@alexdevs.me>
2023-02-15 14:35:44 +0100

    Fix incomplete license on top of each file

--- Capy64/Capy64.cs
+++ Capy64/Capy64.cs
@@ -1,11 +1,17 @@
 ﻿// This file is part of Capy64 - https://github.com/Capy64/Capy64
 // Copyright 2023 Alessandro "AlexDevs" Proto
 //
-//    Licensed under the Apache License, Version 2.0 (the "License").
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License").
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 using Capy64.API;
 using Capy64.Core;
