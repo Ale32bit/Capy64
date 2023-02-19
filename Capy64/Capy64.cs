70ff5ba20b115b16eb1e9c9f102c54925d3e82be
Alessandro Proto <me@alexdevs.me>
2023-02-19 14:10:36 +0100

    Bump to v0.0.9-alpha

--- Capy64/Capy64.cs
+++ Capy64/Capy64.cs
@@ -33,7 +33,7 @@ namespace Capy64;
 
 public class Capy64 : Game, IGame
 {
-    public const string Version = "0.0.8-alpha";
+    public const string Version = "0.0.9-alpha";
     public static string AppDataPath = Path.Combine(
         Environment.GetFolderPath(
             Environment.SpecialFolder.ApplicationData,
